9392c528a1804e7167c12b3f28a1f68d304e43e5
mhmdreza-gp <m78.gholipour@gmail.com>
2022-08-16 04:47:20 -0700

    --

--- pandas_tasks/1st_task.py
+++ pandas_tasks/1st_task.py
@@ -1,5 +1,14 @@
-a = 100
-print(a)
+class rectangle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def area(self):
+        calc_area = self.x * self.y
+        return calc_area
+
+
+v1 = rectangle(2, 3)
+print(v1.area())
 
 
